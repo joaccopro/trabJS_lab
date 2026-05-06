f1bae978e462df1945b4b2021f5c3db8ce2ea3db
joaquinpro <joaquinsanchezma4@gmail.com>
2026-05-06 17:52:36 -0500

    Casi me engaña profesor, justito veo el diagrama y me doy cuenta que esta separado una parte de ella. PILLIN

--- infra/modulos/vpc/main.tf
+++ infra/modulos/vpc/main.tf
@@ -11,7 +11,6 @@ resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
   tags                    = { Name = "public-a-${terraform.workspace}" }
 }
 
@@ -19,7 +18,6 @@ resource "aws_subnet" "public_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
   tags                    = { Name = "public-b-${terraform.workspace}" }
 }
 
