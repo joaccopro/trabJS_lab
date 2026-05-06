500a0d6b4ec6e0388af0bbb0bcf76ec13700b236
joaquinpro <joaquinsanchezma4@gmail.com>
2026-05-06 18:58:05 -0500

    feat(compute): exportar ARNs de lambdas para integracion con API

--- infra/modulos/compute/main.tf
+++ infra/modulos/compute/main.tf
@@ -96,3 +96,11 @@ resource "aws_lambda_function" "crop" {
 
   tags = { Name = "lambda-crop-${terraform.workspace}" }
 }
+
+output "upload_lambda_invoke_arn" {
+  value = aws_lambda_function.upload.invoke_arn
+}
+
+output "upload_lambda_name" {
+  value = aws_lambda_function.upload.function_name
+}
